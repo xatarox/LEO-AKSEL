fc314d9af0eff3499d715bf4d1d43d1153c14b9a
ATAR <65398082+xatarox@users.noreply.github.com>
2021-01-30 18:54:11 +0100

    Update suite Fibonacci.py

--- suite Fibonacci.py
+++ suite Fibonacci.py
@@ -1,1 +1,19 @@
+#suite de fibonacci
 
+
+from time import *
+
+
+a, b, c=1,1,1
+
+while True:
+	print(c, "<>", b, type(b)) #int
+	a,b,c=b,a+b,c+1
+	sleep(0.1)
+
+''''
+while True:
+	print("[+]", b, type(b)) #int
+	a,b=b,a+b
+	sleep(0.1)
+''''
